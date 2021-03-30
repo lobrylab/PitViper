0b77eec83be52bb0a6a81aa911a606e6d645f266
Pantheas <paularth.meslin@gmail.com>
2021-03-30 17:18:22 +0200

    Update pitviper CLI to new config yaml template

--- PitViper/pitviper.py
+++ PitViper/pitviper.py
@@ -1,11 +1,13 @@
 import click
 import os
+import subprocess
+
 
 def run_pitviper(configfile, dry_run):
     if dry_run:
-        cmd = "snakemake -d PitViper/ -s PitViper/workflow/Snakefile -n --configfile {configfile} --use-conda --cores 1".format(configfile=configfile)
+        cmd = "snakemake -s workflow/Snakefile -n --configfile {configfile} --use-conda --cores 1".format(configfile=configfile)
     elif not dry_run:
-        cmd = "snakemake -d PitViper/ -s PitViper/workflow/Snakefile --configfile {configfile} --use-conda --cores 1".format(configfile=configfile)
+        cmd = "snakemake -s workflow/Snakefile --configfile {configfile} --use-conda --cores 1".format(configfile=configfile)
     print('Command:', cmd)
     os.system(cmd)
 
@@ -16,18 +18,18 @@ def run_pitviper(configfile, dry_run):
 @click.option('--dry_run', help='If dry run is enabled, commands won\'t be executed. A message will be printed instead.', default=False, type=bool)
 def main(run_snakemake, configfile, dry_run):
     try:
-        configfile = 'PitViper/{configfile}'.format(configfile=configfile)
-        f = open(configfile)
-        if dry_run:
-            run_pitviper(configfile=configfile, dry_run=True)
-            f.close()
-        elif run_snakemake:
-            run_pitviper(configfile=configfile, dry_run=False)
-            f.close()
-        else:
-            print('Dry run activated or snakemake not activated, pipeline won\'t be executed.')
+        print('Working directory:', os.getcwd())
+        print('Trying to open:', configfile)
+        f = open(configfile, 'r')
+        f.close()
     except IOError:
         print("Error: cannot open and read configuration file {configfile}".format(configfile=configfile))
+    if dry_run:
+        run_pitviper(configfile=configfile, dry_run=True)
+    elif run_snakemake:
+        run_pitviper(configfile=configfile, dry_run=False)
+    else:
+        print('Dry run activated or snakemake not activated, pipeline won\'t be executed.')
 
 
 if __name__ == '__main__':
--- PitViper/pitviper_config.py
+++ PitViper/pitviper_config.py
@@ -4,14 +4,11 @@ import os
 import os.path
 
 class ConfigPitViper:
-    def __init__(self, token, screen_type, count_from, fastq_dir, bam_dir, count_table, count_table_base_name, samples, files, library, mageck_mle_activate, mageck_mle_norm_method, mageck_rra_activate, bagel_activate, bagel_essentials, bagel_nonessential, crisphiermix_activate):
+    def __init__(self, token, screen_type, start_from, count_table, samples, files, library, mageck_mle_activate, mageck_mle_norm_method, mageck_rra_activate, bagel_activate, bagel_essentials, bagel_nonessentials, crisphiermix_activate):
         self.token = token
         self.screen_type = screen_type
-        self.count_from = count_from
-        self.fastq_dir = fastq_dir
-        self.bam_dir = bam_dir
+        self.start_from = start_from
         self.count_table = count_table
-        self.count_table_base_name = count_table_base_name
         self.samples = samples
         self.files = files
         self.library = library
@@ -20,7 +17,7 @@ class ConfigPitViper:
         self.mageck_rra_activate = mageck_rra_activate
         self.bagel_activate = bagel_activate
         self.bagel_essentials = bagel_essentials
-        self.bagel_nonessential = bagel_nonessential
+        self.bagel_nonessentials = bagel_nonessentials
         self.crisphiermix_activate = crisphiermix_activate
 
     def show_config(self):
@@ -29,11 +26,8 @@ class ConfigPitViper:
         print('------------------------------')
         print('Token:\t', self.token)
         print('Screen type:\t', self.screen_type)
-        print('Start from:', self.count_from)
-        print('Fastq files directory:\t', self.fastq_dir)
-        print('Bam files directory:\t', self.bam_dir)
+        print('Start from:', self.start_from)
         print('Count table:\t', self.count_table)
-        print('Count table basename:\t', self.count_table_base_name)
         print('Samples file:\t', self.samples)
         print('Files:\t', self.files)
         print('Library:\t', self.library)
@@ -42,21 +36,18 @@ class ConfigPitViper:
         print('MAGeCK RRA activate:\t', self.mageck_rra_activate)
         print('BAGEL activate:\t', self.bagel_activate)
         print('BAGEL essential genes:\t', self.bagel_essentials)
-        print('BAGEL nonessential genes:\t', self.bagel_nonessential)
+        print('BAGEL nonessential genes:\t', self.bagel_nonessentials)
         print('CRISPhieRmix activate:\t', self.crisphiermix_activate)
         print('------------------------------')
 
 
     def write_yaml_conf(self):
-        file_out_name = 'PitViper/config/{token}.yaml'.format(token=self.token)
+        file_out_name = 'config/{token}.yaml'.format(token=self.token)
         print('> Writing yaml config file:', file_out_name)
         dict_file = {'token' : self.token,
                      'screen_type' : self.screen_type,
-                     'count_from' : self.count_from,
-                     'inputs' : {'fastq_dir' : self.fastq_dir,
-                                 'bam_dir' : self.bam_dir,
-                                 'count_table' : self.count_table,
-                                 'count_table_base_name' : self.count_table_base_name,
+                     'start_from' : self.start_from,
+                     'inputs' : {'count_table' : self.count_table,
                                  'samples' : self.samples,
                                  'files' : self.files,
                                  'library' : self.library},
@@ -65,21 +56,29 @@ class ConfigPitViper:
                                  'RRA' : {'activate' : self.mageck_rra_activate}},
                      'BAGEL' : {'activate' : self.bagel_activate,
                                 'essentials' : self.bagel_essentials,
-                                'nonessential' : self.bagel_nonessential},
+                                'nonessentials' : self.bagel_nonessentials},
                      'CRISPhieRmix' : {'activate' : self.crisphiermix_activate}}
 
         with open(file_out_name, 'w') as file:
             documents = yaml.dump(dict_file, file)
 
+def get_config(form_results):
+    parameters_cmd = ""
+    parameters = ["token", "screen_type", "count_from", "count_table", "samples", "files", "library", "mageck_mle_activate", "mageck_mle_norm_method", "mageck_rra_activate", "bagel_activate", "bagel_essentials", "bagel_nonessentials", "crisphiermix_activate"]
+    for parameter in parameters:
+        if parameter in form_results:
+            value = form_results[parameter]
+        else:
+            value = ""
+        parameters_cmd += "--{param} {value}".format(param=parameter, value=form_results[parameter])
+    return parameters_cmd
+
 
 @click.command()
 @click.option('--token', help='Token.', required=True, type=str)
 @click.option('--screen_type', help='Screen type (eg. "shRNA", "CRISPRi")).', required=True, type=str)
-@click.option('--count_from', help='Count from (eg. "bam", "fastq", "count")).', required=True, type=str)
-@click.option('--fastq_dir', help='fastq directory.', required=True, type=str)
-@click.option('--bam_dir', help='bam directory.', required=True, type=str)
+@click.option('--start_from', help='Count from (eg. "bam", "fastq", "count")).', required=True, type=str)
 @click.option('--count_table', help='Count table.', required=True, type=str)
-@click.option('--count_table_base_name', help='Count table base name.', required=True, type=str)
 @click.option('--samples', help='Samples file.', required=True, type=str)
 @click.option('--files', help='Files file.', required=True, type=str)
 @click.option('--library', help='Library file.', required=True, type=str)
@@ -88,10 +87,10 @@ class ConfigPitViper:
 @click.option('--mageck_rra_activate', help='MAGeCK RRA activate.', required=True, type=str)
 @click.option('--bagel_activate', help='BAGEL activate.', required=True, type=str)
 @click.option('--bagel_essentials', help='BAGEL essential genes.', required=True, type=str)
-@click.option('--bagel_nonessential', help='BAGEL nonessential genes.', required=True, type=str)
+@click.option('--bagel_nonessentials', help='BAGEL nonessential genes.', required=True, type=str)
 @click.option('--crisphiermix_activate', help='CRISPhieRmix activate.', required=True, type=str)
-def main(token, screen_type, count_from, fastq_dir, bam_dir, count_table, count_table_base_name, samples, files, library, mageck_mle_activate, mageck_mle_norm_method, mageck_rra_activate, bagel_activate, bagel_essentials, bagel_nonessential, crisphiermix_activate):
-    config_pitviper = ConfigPitViper(token, screen_type, count_from, fastq_dir, bam_dir, count_table, count_table_base_name, samples, files, library, mageck_mle_activate, mageck_mle_norm_method, mageck_rra_activate, bagel_activate, bagel_essentials, bagel_nonessential, crisphiermix_activate)
+def main(token, screen_type, start_from, count_table, samples, files, library, mageck_mle_activate, mageck_mle_norm_method, mageck_rra_activate, bagel_activate, bagel_essentials, bagel_nonessentials, crisphiermix_activate):
+    config_pitviper = ConfigPitViper(token, screen_type, start_from, count_table, samples, files, library, mageck_mle_activate, mageck_mle_norm_method, mageck_rra_activate, bagel_activate, bagel_essentials, bagel_nonessentials, crisphiermix_activate)
     config_pitviper.show_config()
     yaml_conf_file = 'PitViper/config/{token}.yaml'.format(token=token)
     if os.path.isfile(yaml_conf_file):
