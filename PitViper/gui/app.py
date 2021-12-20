d4ef3fde0a00140aefe543d51b679eaa65bc7e94
PaulArthurM <paularth.meslin@gmail.com>
2021-12-20 02:25:28 +0100

    Folder removal

--- PitViper/gui/app.py
+++ PitViper/gui/app.py
@@ -45,55 +45,54 @@ def result():
         except KeyError:
             library_file = False
 
-        #library_file = request.files.get('library_file')
         tsv_file = request.files.get('tsv_file')
         count_table_file = request.files.get('count_table_file')
 
     if not os.path.exists('resources/' + result_dict['token'] + '/'):
         os.makedirs('resources/' + result_dict['token'] + '/')
 
-        if library_file:
-            library_filename = 'resources/' + result_dict['token'] + '/' + library_file.filename
-            library_file.save(library_filename)
-        else:
-            library_filename = ''
-        result_dict['library_file'] = library_filename
+    if library_file:
+        library_filename = 'resources/' + result_dict['token'] + '/' + library_file.filename
+        library_file.save(library_filename)
+    else:
+        library_filename = ''
+    result_dict['library_file'] = library_filename
 
-        tsv_filename = 'resources/' + result_dict['token'] + '/' +  tsv_file.filename
-        tsv_file.save(tsv_filename)
+    tsv_filename = 'resources/' + result_dict['token'] + '/' +  tsv_file.filename
+    tsv_file.save(tsv_filename)
 
-        result_dict['tsv_file'] = tsv_filename
+    result_dict['tsv_file'] = tsv_filename
 
-        if count_table_file.filename != '':
-            count_table_filename = 'resources/' + result_dict['token'] + '/' + count_table_file.filename
-            count_table_file.save(count_table_filename)
-            result_dict['count_table_file'] = count_table_filename
-            result_dict['normalized_count_table'] = 'resources/' + result_dict['token'] + '/screen.count_normalized.txt'
-        else:
-            result_dict['count_table_file'] = 'resources/' + result_dict['token'] + '/screen.count.txt'
-            result_dict['normalized_count_table'] = 'resources/' + result_dict['token'] + '/screen.count_normalized.txt'
+    if count_table_file.filename != '':
+        count_table_filename = 'resources/' + result_dict['token'] + '/' + count_table_file.filename
+        count_table_file.save(count_table_filename)
+        result_dict['count_table_file'] = count_table_filename
+        result_dict['normalized_count_table'] = 'resources/' + result_dict['token'] + '/screen.count_normalized.txt'
+    else:
+        result_dict['count_table_file'] = 'resources/' + result_dict['token'] + '/screen.count.txt'
+        result_dict['normalized_count_table'] = 'resources/' + result_dict['token'] + '/screen.count_normalized.txt'
 
-        if result_dict['bagel_activate'] == 'False':
-            result_dict['nonessentials'] = ''
-            result_dict['essentials'] = ''
-        else:
-            ess = request.files['essentials']
-            ess_filename = 'resources/' + result_dict['token'] + '/' + ess.filename
-            ess.save(ess_filename)
+    if result_dict['bagel_activate'] == 'False':
+        result_dict['nonessentials'] = ''
+        result_dict['essentials'] = ''
+    else:
+        ess = request.files['essentials']
+        ess_filename = 'resources/' + result_dict['token'] + '/' + ess.filename
+        ess.save(ess_filename)
 
-            non_ess = request.files['nonessentials']
-            noness_filename = 'resources/' + result_dict['token'] + '/' + non_ess.filename
-            non_ess.save(noness_filename)
+        non_ess = request.files['nonessentials']
+        noness_filename = 'resources/' + result_dict['token'] + '/' + non_ess.filename
+        non_ess.save(noness_filename)
 
-            result_dict['nonessentials'] = noness_filename
-            result_dict['essentials'] = ess_filename
+        result_dict['nonessentials'] = noness_filename
+        result_dict['essentials'] = ess_filename
 
-        yaml_file_name = 'config/' + result['token'] + '.yaml'
-        with open(yaml_file_name, 'w') as file:
-          documents = yaml.dump(result_dict, file)
-        run_pitviper(token=result_dict['token'])
-        shutdown_server()
-        return 'Server shutting down...'#render_template("result.html",result = result)
+    yaml_file_name = 'config/' + result['token'] + '.yaml'
+    with open(yaml_file_name, 'w') as file:
+      documents = yaml.dump(result_dict, file)
+    run_pitviper(token=result_dict['token'])
+    shutdown_server()
+    return 'Server shutting down...'#render_template("result.html",result = result)
 
 def open_browser():
       webbrowser.open_new('http://127.0.0.1:5000/')
