d39529b1100744433389c9ed7bc11257b525b067
PaulArthurM <paularth.meslin@gmail.com>
2022-02-15 10:18:20 +0100

    Implement default GSEA ranking metrics

--- PitViper/workflow/scripts/screen_enrichment_analysis.R
+++ PitViper/workflow/scripts/screen_enrichment_analysis.R
@@ -6,9 +6,11 @@ library(dplyr)
 library(readr)
 library(fgsea)
 library(DESeq2)
+library(tibble)
+library(matrixStats)
 
 
-# Handle the arguments.
+# Handle arguments.
 cts_file <- snakemake@input[[1]]
 res_file <- snakemake@input[[2]]
 treatment <- snakemake@params[1]
@@ -19,14 +21,38 @@ baseline <- snakemake@params[2]
 cts <- read.csv(cts_file, sep="\t", row.names="sgRNA", check.names=FALSE)
 cts <- cbind(cts[ , grepl( c(baseline) , names( cts ) ) ], cts[ , grepl( c(treatment) , names( cts ) ) ])
 
+columns <- colnames(cts)
+
+control_columns <- columns[grepl(c(baseline), columns)]
+treatment_columns <- columns[grepl(c(treatment), columns)]
+
+print(control_columns)
+print(treatment_columns)
+
+treatment_df <- cts %>% select(treatment_columns)
+control_df <- cts %>% select(control_columns)
+
+table <- data.frame(mean_treatment=rowMeans(treatment_df[,]),
+           mean_control=rowMeans(control_df[,]),
+           sd_treatment = rowSds(as.matrix(treatment_df[,])),
+           sd_control = rowSds(as.matrix(control_df[,]))) %>%
+  mutate(rank_metric = (mean_treatment - mean_control) / (sd_treatment + sd_control)) %>%
+  arrange(rank_metric) %>%
+  rownames_to_column("sgRNA") %>%
+  filter(!is.na(rank_metric)) %>%
+  select(rank_metric, sgRNA)
+
+ranking <- setNames(table$rank_metric, table$sgRNA)
+
+print(head(ranking))
 
 # Open and read DESeq2 output file.
-res <- read_delim(res_file, "\t", escape_double = FALSE, trim_ws = TRUE)
+# res <- read_delim(res_file, "\t", escape_double = FALSE, trim_ws = TRUE)
 
 
 # Rank sgRNAs using p-value and log2Fold-Change.
-res <- res %>% mutate(rank_metric = log2FoldChange) %>% filter(!is.na(rank_metric)) %>% select(rank_metric, sgRNA)
-ranking <- setNames(res$rank_metric, res$sgRNA)
+# res <- res %>% mutate(rank_metric = log2FoldChange) %>% filter(!is.na(rank_metric)) %>% select(rank_metric, sgRNA)
+# ranking <- setNames(res$rank_metric, res$sgRNA)
 
 
 
