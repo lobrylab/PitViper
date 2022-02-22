3862513a2e4af1172f63398e54860596ea5e4e36
PaulArthurM <paularth.meslin@gmail.com>
2022-02-22 07:42:57 +0100

    Update run_crisphiermix.R

--- PitViper/workflow/scripts/run_crisphiermix.R
+++ PitViper/workflow/scripts/run_crisphiermix.R
@@ -21,20 +21,30 @@ res <- read_delim(snakemake@input[[1]],
 cts <- read_delim(snakemake@input[[2]],
                           "\t", escape_double = FALSE, trim_ws = TRUE)
 
+# Annotate DESeq2 data with elements name
 genes <- cts %>% select(Gene, sgRNA)
 all_count.DESeq2 <- merge(x = genes, y = res, by = "sgRNA")
 
 
-neg_controls_file <- snakemake@params[[3]]
-neg_controls_ids <- read.csv(neg_controls_file, header = F, col.names = "id")$id
+if (snakemake@params[[3]] != "") {
+  # Open negatives controls guides file
+  neg_controls_file <- snakemake@params[[3]]
+  # Retrieve neg controls guides ID
+  neg_controls_ids <- read.csv(neg_controls_file, header = F, col.names = "id")$id
+  # Retrieve neg controls guides log2FoldChange
+  neg_ctrl_guides <- which(all_count.DESeq2$sgRNA %in% neg_controls_ids)
+  neg_ctrl_guides_log2fc <- all_count.DESeq2 %>% filter(sgRNA %in% neg_controls_ids) %>% pull(log2FoldChange)
 
-negCtrlGuides <- which(all_count.DESeq2$sgRNA %in% neg_controls_ids)
-
-
-log2fc = all_count.DESeq2$log2FoldChange
-log2fc.cleaned <- all_count.DESeq2[which(!is.na(all_count.DESeq2$log2FoldChange)),]$log2FoldChange
-geneIds = all_count.DESeq2[which(!is.na(all_count.DESeq2$log2FoldChange)),]$Gene
-geneIds = factor(geneIds, levels = unique(geneIds))
+  # Retrieve non controls guides log2FoldChanges
+  all_count.DESeq2 <- all_count.DESeq2 %>% filter(!sgRNA %in% neg_controls_ids) %>% filter(!is.na(log2FoldChange))
+  geneIds = all_count.DESeq2$Gene
+  geneIds = factor(geneIds, levels = unique(geneIds))
+} else {
+  # Retrieve non controls guides log2FoldChanges
+  all_count.DESeq2 <- all_count.DESeq2 %>% filter(!is.na(log2FoldChange))
+  geneIds = all_count.DESeq2$Gene
+  geneIds = factor(geneIds, levels = unique(geneIds))
+}
 
 
 ### Compute mean log2FoldChange of Top-3 sgRNAs per target.
@@ -44,9 +54,12 @@ meanlog2FoldChanges <- all_count.DESeq2 %>%
   summarise(top_3_mean_log2FoldChange = mean(log2FoldChange))
 
 
-
 ### Run CRISPhieRmix
-log2fcCRISPhieRmixFit = CRISPhieRmix::CRISPhieRmix(log2fc.cleaned, geneIds = geneIds)
+if (snakemake@params[[3]] != "") {
+  log2fcCRISPhieRmixFit = CRISPhieRmix::CRISPhieRmix(x = all_count.DESeq2$log2FoldChange, geneIds = geneIds,  negCtrl = neg_ctrl_guides_log2fc)
+} else {
+  log2fcCRISPhieRmixFit = CRISPhieRmix::CRISPhieRmix(x = all_count.DESeq2$log2FoldChange, geneIds = geneIds)
+}
 log2fcCRISPhieRmixScores = data.frame(gene = log2fcCRISPhieRmixFit$genes, locfdr = log2fcCRISPhieRmixFit$locfdr, score = log2fcCRISPhieRmixFit$geneScores, FDR = log2fcCRISPhieRmixFit$FDR)
 log2fcCRISPhieRmixScores$locfdr[which(log2fcCRISPhieRmixScores$FDR < 0)] = 0
 log2fcCRISPhieRmixScores = log2fcCRISPhieRmixScores[order(log2fcCRISPhieRmixScores$locfdr, decreasing = FALSE), ]
