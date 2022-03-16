654d37ebebed462786514a525121b20a08ad58c2
PaulArthurM <paularth.meslin@gmail.com>
2022-03-16 18:13:01 +0100

    Update pitviper.sh

--- pitviper.sh
+++ pitviper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
 ### Activation of pitviper_env.
-conda activate pitviper_test
+conda activate pitviper_env
 
 ### Run PitViper application with Flask.
 (cd ./PitViper && python3 gui/waitress_server.py)
