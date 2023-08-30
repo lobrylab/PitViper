864aa5a610531ad2cc1b6269e0b614c7a4f50974
PaulArthurM <paularth.meslin@gmail.com>
2023-08-30 18:24:21 +0200

    Add lock file option to installation script

--- run.sh
+++ run.sh
@@ -2,27 +2,46 @@
 
 # This script sets up the environment and runs the PitViper Flask application
 
-# Get first argument
-installer=$1
-
-# Check if installer is 'conda' or 'mamba'
-if [[ "$installer" == "conda" ]]; then
-    echo "Using conda installer"
-elif [[ "$installer" == "mamba" ]]; then
-    echo "Using mamba installer"
-else
-    # Set default installer to conda
-    installer="mamba"
-    echo "No installer specified. Using mamba installer by default."
-fi
-
 # Exit if any command fails
 set -e
+
+# Constants for installer and mode
+readonly CONDA_INSTALLER="conda"
+readonly MAMBA_INSTALLER="mamba"
+readonly YAML_MODE="yaml"
+readonly LOCK_MODE="lock"
+
+# Initialize installer variable before conditional checks
+installer="${1:-$MAMBA_INSTALLER}"
+mode="${2:-$YAML_MODE}"
 
 # Set configurable variables using environment variables
 app_env_var=${APP_ENV_VAR:-FLASK_APP}
 conda_env_name=${CONDA_ENV_NAME:-pitviper_env}
 app_path=${APP_PATH:-PitViper/gui/app.py}
+
+# Check if installer is 'conda' or 'mamba'
+if [[ "$installer" == "$CONDA_INSTALLER" ]]; then
+    echo "Using conda installer"
+elif [[ "$installer" == "$MAMBA_INSTALLER" ]]; then
+    echo "Using mamba installer"
+else
+    # Set default installer to conda
+    installer="$MAMBA_INSTALLER"
+    echo "No installer specified. Using mamba installer by default."
+fi
+
+# Check if mode is 'yaml' or 'lock'
+if [[ "$mode" == "$YAML_MODE" ]]; then
+    echo "Using PitViper/environment.yaml file"
+elif [[ "$mode" == "$LOCK_MODE" ]]; then
+    echo "Using PitViper/environment_lock.yaml file"
+else
+    # Set default mode to yaml
+    mode="$YAML_MODE"
+    echo "No mode specified. Using PitViper/environment.yaml file by default."
+fi
+
 
 # Check that the required file exists
 test -e "$app_path" || { echo "Error: $app_path does not exist"; exit 1; }
@@ -34,30 +53,56 @@ app_full_path=$(realpath "$app_path")
 if conda env list | grep -qsw "$conda_env_name"; then
     echo "Activating conda environment: $conda_env_name"
     conda_path=$(conda info | grep -i 'base environment' | awk '{print $4}')
+
+    # Activate conda environment
     source $conda_path/etc/profile.d/conda.sh
     conda activate "$conda_env_name"
 else
+
     # Create conda environment
     echo "Creating conda environment: $conda_env_name"
+
     # Check if installer is 'conda' or 'mamba'
-    if [[ "$installer" == "conda" ]]; then
+    if [[ "$installer" == "$CONDA_INSTALLER" ]]; then
+
         # Install packages using conda
         echo "Installing packages using conda..."
-        conda env create -f PitViper/environment.yaml -n "$conda_env_name"
-    elif [[ "$installer" == "mamba" ]]; then
+        if [[ "$mode" == "$YAML_MODE" ]]; then
+
+            # Install packages using conda and environment.yaml file
+            conda env create -f PitViper/environment.yaml -n "$conda_env_name"
+        elif [[ "$mode" == "$LOCK_MODE" ]]; then
+
+            # Install packages using conda and environment.lock file
+            conda create --file PitViper/environment_lock.yaml -n "$conda_env_name"
+        fi
+    elif [[ "$installer" == "$MAMBA_INSTALLER" ]]; then
+
         # Install packages using mamba
         echo "Installing packages using mamba..."
 
         # Check if mamba package is already installed
         if conda list -n base | grep -qws "mamba"; then
             echo "Mamba package is already installed."
+
         # Install mamba package if it's not already installed
         else
             echo "Installing Mamba package..."
             conda install -y -c conda-forge mamba
         fi
-        mamba env create -f PitViper/environment.yaml -n "$conda_env_name"
+
+        # Check if mode is 'yaml' or 'lock'
+        if [[ "$mode" == "$YAML_MODE" ]]; then
+
+            # Install packages using mamba and environment.yaml file
+            mamba env create -f PitViper/environment.yaml -n "$conda_env_name"
+        elif [[ "$mode" == "lock" ]]; then
+
+            # Install packages using mamba and environment.lock file
+            mamba create --file PitViper/environment_lock.yaml -n "$conda_env_name"
+        fi
     fi
+    # Activate conda environment
     conda_path=$(conda info | grep -i 'base environment' | awk '{print $4}')
     source $conda_path/etc/profile.d/conda.sh
     conda activate "$conda_env_name"
@@ -68,7 +113,11 @@ else
     rm -rf PitViper/CRISPhieRmix.tar.gz
     
     # Install BAGEL2
-    git clone https://github.com/hart-lab/bagel.git PitViper/workflow/scripts/bagel
+    # Check if PitViper/workflow/scripts/bagel already exists, if not clone the repository
+    bagel_dir="PitViper/workflow/scripts/bagel"
+    if [[ ! -d "$bagel_dir" ]]; then
+        git clone https://github.com/hart-lab/bagel.git "$bagel_dir"
+    fi
 
     # Freeze the conda environment to a YAML file
     conda env export > PitViper/environment_freeze.yaml
